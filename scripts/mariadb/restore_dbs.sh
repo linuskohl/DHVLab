8f4675d6f4ec1b686a2093493566eafb9a211b50
linuskohl <linus.kohl@live.com>
2017-10-16 18:04:58 +0200

    Add DB restore script

---
+++ scripts/mariadb/restore_dbs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Quick fix to import Database Backups
+#2017_10_10 Linus Kohl
+for db_export in *.mysql; do
+    db_name=`echo $db_export | sed -e 's/\(.mysql\)*$//g'`
+    echo $db_name
+    mysql -uroot -e "create database \`$db_name\`"
+    mysql -uroot $db_name < $db_import
+done
