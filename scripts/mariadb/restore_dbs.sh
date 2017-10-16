d53f9081cc981921395428ff4aa0df344cc6d76f
linuskohl <linus.kohl@live.com>
2017-10-16 18:07:05 +0200

    Add DB restore script

--- scripts/mariadb/restore_dbs.sh
+++ scripts/mariadb/restore_dbs.sh
@@ -5,5 +5,5 @@ for db_export in *.mysql; do
     db_name=`echo $db_export | sed -e 's/\(.mysql\)*$//g'`
     echo $db_name
     mysql -uroot -e "create database \`$db_name\`"
-    mysql -uroot $db_name < $db_import
+    mysql -uroot $db_name < $db_export
 done
