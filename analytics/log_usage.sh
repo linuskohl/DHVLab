ea834a9302a9f1f8955ea0020d76ffa63da2dd65
Linus Kohl <linus@munichresearch.com>
2017-12-07 15:35:49 +0100

    Create directory of logfile

--- analytics/log_usage.sh
+++ analytics/log_usage.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #(c) Linus Kohl <kohl@munichresearch.com>
 
+LOGFILE='/var/log/stats/user_stats.log'
 TSTAMP=$(date +%s)
 NRUSERS=$(users |wc -w)
 USERS=$(users | tr ' ' ';')
 LOAD=$(sar -u 2 1 |tail -n1 |awk '{print $5}' |tr ',' '.' )
 MEMORY=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-echo $TSTAMP,$LOAD,$MEMORY,$NRUSERS,\"$USERS\" >> /var/log/statistics.log
+mkdir -p `dirname $LOGFILE`
+echo $TSTAMP,$LOAD,$MEMORY,$NRUSERS,\"$USERS\" >> $LOGFILE
 
